c4def430fd4de7380b51c1ff6f7fb1243dcb25b7
Kari Groszewska <karolina.groszewska@gmail.com>
2025-07-07 19:53:52 -0500

    Part 1 Progress

--- PetPlus/Item.swift
+++ PetPlus/Item.swift
@@ -5,14 +5,15 @@
 //  Created by Kari Groszewska on 7/5/25.
 //
 
-import Foundation
-import SwiftData
-
-@Model
-final class Item {
-    var timestamp: Date
-    
-    init(timestamp: Date) {
-        self.timestamp = timestamp
-    }
-}
+//TODO: Add structure for our DB and rename accordinglu
+//import Foundation
+//import SwiftData
+//
+//@Model
+//final class Item {
+//    var timestamp: Date
+//    
+//    init(timestamp: Date) {
+//        self.timestamp = timestamp
+//    }
+//}
